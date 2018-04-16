0461089ff8a380b87abb97c09e0666f2f212669e
Olga Abir <olga_abir@yahoo.com>
2018-04-16 19:37:40 +0300

    added empty project

---
+++ UserAuthentication/app/src/main/java/idan/dean/olga/userauthentication/LoginScreen.java
@@ -0,0 +1,13 @@
+package idan.dean.olga.userauthentication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_screen);
+    }
+}
