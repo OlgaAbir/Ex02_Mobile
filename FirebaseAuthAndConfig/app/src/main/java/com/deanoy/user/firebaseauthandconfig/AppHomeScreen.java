f596c9164ad66193e057497efc9c2088dba84d0d
Olga Abir <olga_abir@yahoo.com>
2018-04-18 20:36:12 +0300

    google sign in works, basic user email-password auth and sign up works,
    added HomeScreen basic implementation

--- FirebaseAuthAndConfig/app/src/main/java/com/deanoy/user/firebaseauthandconfig/AppHomeScreen.java
+++ FirebaseAuthAndConfig/app/src/main/java/com/deanoy/user/firebaseauthandconfig/AppHomeScreen.java
@@ -1,13 +1,74 @@
 package com.deanoy.user.firebaseauthandconfig;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class AppHomeScreen extends Activity {
 
+    private static String TAG = "AppHomeScreen";
+
+    private FirebaseAuth mAuth;
+    private FirebaseUser mLoggedInUser;
+    private TextView mUserName;
+    private ImageView mUserProfilePicture;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_home_screen);
+
+        // binding and initialization
+        mAuth = FirebaseAuth.getInstance();
+        mUserName =  findViewById(R.id.tvUserName);
+        mUserProfilePicture = findViewById(R.id.ivProfilePicture);
+        mLoggedInUser = mAuth.getCurrentUser();
+
+        displayLoggedInUserProfile();
+    }
+
+    private void displayLoggedInUserProfile()
+    {
+        if(mLoggedInUser != null)
+        {
+            if(mLoggedInUser.getDisplayName() == null || mLoggedInUser.getDisplayName().isEmpty())
+            {
+                mUserName.setText("Display Name: unconfigured");
+            }
+            else
+            {
+                mUserName.setText(mLoggedInUser.getDisplayName());
+            }
+
+            if(mLoggedInUser.getPhotoUrl() != null)
+            {
+                mUserProfilePicture.setImageURI(mLoggedInUser.getPhotoUrl());
+            }
+        }
+    }
+
+    public void onSignOutClick(View v)
+    {
+        Log.e(TAG, "onSignoutClick >>");
+
+        mAuth.signOut();
+        startDisplayLoginScreen();
+
+        Log.e(TAG, "onSignoutClick <<");
+    }
+
+    private void startDisplayLoginScreen()
+    {
+        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+
+        startActivity(i);
     }
 }
--- FirebaseAuthAndConfig/app/src/main/java/com/deanoy/user/firebaseauthandconfig/MainActivity.java
+++ FirebaseAuthAndConfig/app/src/main/java/com/deanoy/user/firebaseauthandconfig/MainActivity.java
@@ -26,9 +26,11 @@ import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -40,7 +42,6 @@ public class MainActivity extends Activity {
     private FirebaseAuth mAuth;
     private EditText metEmail;
     private EditText metPassword;
-    private TextView mtvUserDetails;
     private GoogleSignInOptions mGoogleSignInOptions;
     private GoogleSignInClient mGoogleSignInClient;
     private SignInButton mGoogleSignInButton;
@@ -52,7 +53,6 @@ public class MainActivity extends Activity {
         // Bind UI
         this.metEmail = findViewById(R.id.etEmail);
         this.metPassword = findViewById(R.id.etPassword);
-        this.mtvUserDetails = findViewById(R.id.tvUserDetails);
         this.mAuth = FirebaseAuth.getInstance();
         this.mGoogleSignInButton = findViewById(R.id.btnGoogleSignIn);
         this.mGoogleSignInButton .setSize(SignInButton.SIZE_STANDARD);
@@ -69,6 +69,7 @@ public class MainActivity extends Activity {
         // Configure sign-in to request the user's ID, email address, and basic
         // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
         mGoogleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
         // Build a GoogleSignInClient with the options specified by gso.
@@ -76,24 +77,16 @@ public class MainActivity extends Activity {
     }
 
     @Override
-    public void onStart() {
+    public void onStart()
+    {
         super.onStart();
+
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
         this.updateUserDetailsUI(currentUser);
     }
 
     // On Clicks
-    public void onSignOutClick(View v)
-    {
-        Log.e(TAG, "onSignoutClick >>");
-
-        mAuth.signOut();
-        this.updateUserDetailsUI(mAuth.getCurrentUser());
-
-        Log.e(TAG, "onSignoutClick <<");
-    }
-
     public void onSignUpClick(View v)
     {
         Log.e(TAG, "onSignUpClick >>");
@@ -105,12 +98,15 @@ public class MainActivity extends Activity {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             Log.e(TAG, "on mAuth signup complete >>");
-                            if (task.isSuccessful()) {
+                            if (task.isSuccessful())
+                            {
                                 // Sign in success, update UI with the signed-in user's information
                                 Log.e(TAG, "createUserWithEmail:success");
                                 FirebaseUser user = mAuth.getCurrentUser();
                                 updateUserDetailsUI(user);
-                            } else {
+                            }
+                            else
+                            {
                                 // If sign in fails, display a message to the user.
                                 Log.e(TAG, "createUserWithEmail:failure", task.getException());
                                 Toast.makeText(getApplicationContext(), "Authentication failed.",
@@ -133,18 +129,21 @@ public class MainActivity extends Activity {
             mAuth.signInWithEmailAndPassword(metEmail.getText().toString(), metPassword.getText().toString())
                     .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                         @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
+                        public void onComplete(@NonNull Task<AuthResult> task)
+                        {
+                            if (task.isSuccessful())
+                            {
                                 // Sign in success, update UI with the signed-in user's information
                                 Log.e(TAG, "signInWithEmail:success");
                                 FirebaseUser user = mAuth.getCurrentUser();
                                 updateUserDetailsUI(user);
-                            } else {
+                            }
+                            else
+                            {
                                 // If sign in fails, display a message to the user.
                                 Log.e(TAG, "signInWithEmail:failure", task.getException());
                                 Toast.makeText(MainActivity.this, "Authentication failed.",
                                         Toast.LENGTH_SHORT).show();
-                                updateUserDetailsUI(null);
                             }
 
                             // ...
@@ -154,17 +153,10 @@ public class MainActivity extends Activity {
         Log.e(TAG, "onSignInClick <<");
     }
 
-    //Helper functions
-    private void updateUserDetailsUI(FirebaseUser currentUser) {
-        if(currentUser == null) {
-            mtvUserDetails.setText("Signed out");
-        } else {
-            mtvUserDetails.setText(String.format("Email: %s\n Signed in", currentUser.getDisplayName()));
-        }
-    }
-
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
         super.onActivityResult(requestCode, resultCode, data);
+
         Log.e(TAG, "onActivityResult >>");
         Log.e(TAG, "Result code: " + resultCode + ". Request code: " + requestCode);
 
@@ -175,22 +167,60 @@ public class MainActivity extends Activity {
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             handleSignInResult(task);
         }
+
         Log.e(TAG, "onActivityResult <<");
+    }
+
+    //Helper functions
+    private void updateUserDetailsUI(FirebaseUser currentUser)
+    {
+        if(currentUser != null)
+        {
+            startDisplayHomeScreen();
+        }
+    }
+
+    private void startDisplayHomeScreen()
+    {
+        Intent i = new Intent(getApplicationContext(), AppHomeScreen.class);
+
+        startActivity(i);
     }
 
     private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
         try {
             GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-
+            firebaseAuthWithGoogle(account);
             // Signed in successfully, show authenticated UI.
-            Log.e(TAG, "signInResult:success");
-            updateUserDetailsUI(mAuth.getCurrentUser());
+            Log.e(TAG, "signInResult:successAccount");
         } catch (ApiException e) {
             // The ApiException status code indicates the detailed failure reason.
             // Please refer to the GoogleSignInStatusCodes class reference for more information.
             Log.e(TAG, "signInResult:failed code=" + e.getStatusCode());
-            updateUserDetailsUI(null);
         }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + account.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUserDetailsUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 
     private void displayMessage(String message)
@@ -223,7 +253,7 @@ public class MainActivity extends Activity {
         if(result && !isValidPassword())
         {
             displayMessage("You should enter a password that contains at least 6 characters");
-            result = false;
+
         }
         return result;
     }
