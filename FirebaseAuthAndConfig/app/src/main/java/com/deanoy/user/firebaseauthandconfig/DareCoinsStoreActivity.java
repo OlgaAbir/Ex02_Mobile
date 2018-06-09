384cd1abead909fd2e5ebd99e909e3506e896147
Olga Abir <olga_abir@yahoo.com>
2018-06-09 09:28:44 +0300

    finished infrastructure for dare coins store- only left to load from db the dare coins products

--- FirebaseAuthAndConfig/app/src/main/java/com/deanoy/user/firebaseauthandconfig/DareCoinsStoreActivity.java
+++ FirebaseAuthAndConfig/app/src/main/java/com/deanoy/user/firebaseauthandconfig/DareCoinsStoreActivity.java
@@ -6,6 +6,11 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import Models.Dare;
+import Models.DareCoins;
 import Models.DareCoinsAdapter;
 import Models.DaresAdapter;
 
@@ -13,6 +18,7 @@ public class DareCoinsStoreActivity extends Activity {
 
     private RecyclerView mDareCoinsProductsView;
     private DareCoinsAdapter mDareCoinsAdapter;
+    private List<DareCoins> mDareCoinsList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +31,20 @@ public class DareCoinsStoreActivity extends Activity {
         mDareCoinsProductsView.setHasFixedSize(true);
         mDareCoinsProductsView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
         mDareCoinsProductsView.setItemAnimator(new DefaultItemAnimator());
+
+        getAllCoinsProducts();
+    }
+
+    private void getAllCoinsProducts() {
+        mDareCoinsList.clear();
+        mDareCoinsAdapter = new DareCoinsAdapter(mDareCoinsList);
+        mDareCoinsProductsView.setAdapter(mDareCoinsAdapter);
+
+        getDareCoinsFromDB();
+    }
+
+    private void getDareCoinsFromDB()
+    {
+
     }
 }
