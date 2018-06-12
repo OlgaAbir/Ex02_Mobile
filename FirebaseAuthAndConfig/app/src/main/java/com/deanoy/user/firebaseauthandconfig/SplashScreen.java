9258ecedaac8d3091d77815629f0c70c499fa873
Olga Abir <olga_abir@yahoo.com>
2018-06-12 12:02:10 +0300

    added version display at splash screen and signed the app

--- FirebaseAuthAndConfig/app/src/main/java/com/deanoy/user/firebaseauthandconfig/SplashScreen.java
+++ FirebaseAuthAndConfig/app/src/main/java/com/deanoy/user/firebaseauthandconfig/SplashScreen.java
@@ -2,9 +2,12 @@ package com.deanoy.user.firebaseauthandconfig;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.widget.TextView;
 
 public class SplashScreen extends Activity {
 
@@ -19,6 +22,16 @@ public class SplashScreen extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
         setTitle("");
+
+        try {
+            PackageInfo pInfo = this.getPackageManager().getPackageInfo(getPackageName(), 0);
+            TextView tvVersion = findViewById(R.id.tvVersionInfo);
+
+            tvVersion.setText(pInfo.versionName + "."+ String.valueOf(pInfo.versionCode));
+
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
 
         new Handler().postDelayed(new Runnable() {
             @Override
